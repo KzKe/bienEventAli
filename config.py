356363ca649fbf68415201aa65827bc8818c4652
KzKe <kangk1@student.unimelb.edu.au>
2020-05-06 17:50:37 +0800

    add nfold

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-def Config:
-	TRAINING_FILE = 'event_entity_train_data_label.txt'
-	DEV_FILE = 'event_entity_dev_data.txt'
+class Config:
+	TRAINING_FILE = '/content/drive/My Drive/CCKS2020_事件主体抽取/event_entity_train_data_label.txt'
+	DEV_FILE = '/content/drive/My Drive/CCKS2020_事件主体抽取/event_entity_dev_data.txt'
--- read_data.py
+++ read_data.py
@@ -1,11 +1,22 @@
 import numpy as np
 import pandas as pd 
 from config import Config
+from sklearn import preprocessing
+from nfold import CrossValidation
+
 
 train_df = pd.read_csv(Config.TRAINING_FILE ,delimiter="\t", 
                                   names=["ID", "txt", "type", "company"])
-
 dev_df = pd.read_csv(Config.DEV_FILE,delimiter="\t", 
                                   names=["ID", "txt", "type", "company"])
 
+train_df = train_df.dropna()
 
+le = preprocessing.LabelEncoder()
+le.fit(train_df['type'])
+types = le.transform(train_df['type'])
+train_df['event_type'] = types
+
+cv = CrossValidation(train_df, shuffle=True, target_cols=["type"], 
+                      problem_type="multiclass_classification")
+df_split = cv.split().reindex()
